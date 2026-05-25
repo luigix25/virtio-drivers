coconut-svsm/svsm

virtio-drivers/connectionmanager: reject operations after peer shutdown
`send()` and `update_credit()` do not check whether the peer has
requested shutdown, allowing operations on a connection that the peer
considers closed.

Check `peer_requested_shutdown` before proceeding in both methods.

Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

virtio-drivers/src/device/socket/connectionmanager.rs
@@ -152,6 +152,9 @@ impl<H: Hal, T: Transport, const RX_BUFFER_SIZE: usize>
     /// Sends the buffer to the destination.
     pub fn send(&mut self, destination: VsockAddr, src_port: u32, buffer: &[u8]) -> Result {
         let (_, connection) = get_connection(&mut self.connections, destination, src_port)?;
+        if connection.peer_requested_shutdown {
+            return Err(SocketError::NotConnected.into());
+        }
 
         self.driver.send(buffer, &mut connection.info)
     }
@@ -281,6 +284,10 @@ impl<H: Hal, T: Transport, const RX_BUFFER_SIZE: usize>
     /// Sends a credit update to the given peer.
     pub fn update_credit(&mut self, peer: VsockAddr, src_port: u32) -> Result {
         let (_, connection) = get_connection(&mut self.connections, peer, src_port)?;
+        if connection.peer_requested_shutdown {
+            return Err(SocketError::NotConnected.into());
+        }
+
         self.driver.credit_update(&connection.info)
     }
 

luigix25/virtio-drivers

src/device/socket/connectionmanager.rs
@@ -154,6 +154,9 @@ impl<H: Hal, T: Transport, const RX_BUFFER_SIZE: usize>
     /// Sends the buffer to the destination.
     pub fn send(&mut self, destination: VsockAddr, src_port: u32, buffer: &[u8]) -> Result {
         let (_, connection) = get_connection(&mut self.connections, destination, src_port)?;
+        if connection.peer_requested_shutdown {
+            return Err(SocketError::PeerSocketShutdown.into());
+        }
 
         self.driver.send(buffer, &mut connection.info)
     }
@@ -283,6 +286,10 @@ impl<H: Hal, T: Transport, const RX_BUFFER_SIZE: usize>
     /// Sends a credit update to the given peer.
     pub fn update_credit(&mut self, peer: VsockAddr, src_port: u32) -> Result {
         let (_, connection) = get_connection(&mut self.connections, peer, src_port)?;
+        if connection.peer_requested_shutdown {
+            return Err(SocketError::PeerSocketShutdown.into());
+        }
+
         self.driver.credit_update(&connection.info)
     }
 